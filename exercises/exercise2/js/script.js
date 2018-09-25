a20b1d349a2af50618e83124991cb7778b30ca0d
Dana Ryashy <danriachi@hotmail.com>
2018-09-25 16:47:14 -0400

    Ex2: made canvas unscrollable

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -84,7 +84,7 @@ function preload() {
 // Make the canvas, initialise pi variables, position the avatar and enemy
 function setup() {
   // Create our playing area
-  createCanvas(500,500);
+  createCanvas(500, 500);
 
   //Initialize the pi variables
   pi = -PI;
