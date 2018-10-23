1941c40f2927f1bddf0918fe88d56841c5e20f3f
Dana Ryashy <danriachi@hotmail.com>
2018-10-23 15:46:46 -0400

    Ex6: Corrected 11 errors in Paddle.js, and corrected a correction in Ball.js

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -3,17 +3,17 @@
 // A class that defines how a paddle behaves, including the ability
 // to specify the input keys to move it up and down
 
-Paddle constructor
-
-Sets the properties with the provided arguments or defaults
-function Pladdle(x,y,w,h,speed,downKey,upKey) {
+//Paddle constructor
+//
+//Sets the properties with the provided arguments or defaults
+function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.x = x;
   this.y = y;
   this.xv = 0;
   this.yv = 0;
   this.w = w;
   this.h = h;
-  this.speed = speeed;
+  this.speed = speed;
   this.downKey = downKey;
   this.upKey = upKey;
 }
@@ -22,12 +22,12 @@ function Pladdle(x,y,w,h,speed,downKey,upKey) {
 //
 // Check if the up or down keys are pressed and update velocity
 // appropriately
-Paddle.proto.handleInput = function() {
-  if (keyDown(upKey)) {
+Paddle.prototype.handleInput = function() {
+  if (keyIsDown(upKey)) {
     this.vy = -this.speed;
   }
-  else if (keyDown(downKey)) {
-    this.vy = -this.speed;
+  else if (keyIsDown(downKey)) {
+    this.vy = this.speed;
   }
 }
 
@@ -36,12 +36,12 @@ Paddle.proto.handleInput = function() {
 // Constrain the resulting position to be within the canvas
 Paddle.prototype.update = function() {
   this.y += this.vy;
-  this.y = constraint(this.y,0,hight-this.h);
+  this.y = constrain(this.y,0,hight-this.h);
 }
 
 // display()
 //
 // Draw the paddle as a rectangle on the screen
-Paddle.prototype.disploy = function()) {
-  rectangle(this.x,this.y,this.w,this.h);
+Paddle.prototype.display = function() {
+  rect(this.x,this.y,this.w,this.h);
 }
