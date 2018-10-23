d0dbb1b8b1887b2327c6f9d5155093e6910be238
Dana Ryashy <danriachi@hotmail.com>
2018-10-23 17:03:02 -0400

    Ex6: "reset" method was badly written

--- exercises/exercise-6/js/Ball.js
+++ exercises/exercise-6/js/Ball.js
@@ -83,7 +83,8 @@ Ball.prototype.handleCollision = function(paddle) {
 // reset()
 //
 // Set position back to the middle of the screen
-Ball.prototype.rest = function () {
+////////FIXED
+Ball.prototype.reset = function () {
   this.x = width/2;
   this.y = height/2;
 }
