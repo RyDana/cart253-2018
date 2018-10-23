6b4f155da24af83ccecfbdf1139b1afd8e6b64a2
Dana Ryashy <danriachi@hotmail.com>
2018-10-23 16:39:41 -0400

    Ex6: "=" is not a verification of equivalecy

--- exercises/exercise-6/js/Ball.js
+++ exercises/exercise-6/js/Ball.js
@@ -31,7 +31,8 @@ Ball.prototype.updated = function () { ////////Fixed
   this.y = constrain(this.y,0,height-this.size);
 
   // Check for touching upper or lower edge and reverse velocity if so
-  if (this.y = 0 || this.y + this.size === height) {
+  /////////FIXED
+  if (this.y === 0 || this.y + this.size === height) {
     this.vy = -this.vy;
   }
 }
