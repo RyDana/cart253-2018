db6bd2c0096ce871a4dd815301db99f86072d03a
Dana Ryashy <danriachi@hotmail.com>
2018-10-30 17:13:00 -0400

    P2: Created an intro

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -33,6 +33,17 @@ var beepSFX;
 var pointSound;
 var levelUpSound;
 
+///////NEW////////
+//Intro variables
+var introPlaying = true; //detects when intro should stop being played
+var myFont; //font for text display
+var titleText = "PING PONG FOREVER";
+var startGameText = "PRESS X TO START THE GAME"
+var textOpacity = 0; //text will have a changing opacity for fade-in effect
+var xPressed = false; //Detects when the x key is pressed to start game
+var coundownTimer = 180; //A countdown of 180ms plays before game starts
+///////END NEW////////
+
 // preload()
 //
 // Loads the beep audio for the sound of bouncing
@@ -40,6 +51,8 @@ function preload() {
   beepSFX = new Audio("assets/sounds/beep.wav");
   pointSound = new Audio("assets/sounds/point.wav");
   levelUpSound = new Audio("assets/sounds/level-up.wav");
+
+  myFont = loadFont('assets/fonts/FontdinerdotcomHuggable.ttf');
 }
 
 // setup()
@@ -60,35 +73,41 @@ function setup() {
 
 // draw()
 //
-// Handles input, updates all the elements, checks for collisions
+// Plays intro, handles input, updates all the elements, checks for collisions
 // and displays everything.
 function draw() {
   background(0, 100);
 
-  leftPaddle.handleInput();
-  rightPaddle.handleInput();
+  /////NEW///////
+  if (introPlaying){
+    displayIntro(); //Starting with an intro
+  } else {
+  ////////END NEW///////
+    leftPaddle.handleInput();
+    rightPaddle.handleInput();
 
-  ball.update();
-  leftPaddle.update();
-  rightPaddle.update();
+    ball.update();
+    leftPaddle.update();
+    rightPaddle.update();
 
-  checkForScores();
+    checkForScores();
 
-  ball.handleCollision(leftPaddle);
-  ball.handleCollision(rightPaddle);
+    ball.handleCollision(leftPaddle);
+    ball.handleCollision(rightPaddle);
 
-  ball.display();
-  leftPaddle.display();
-  rightPaddle.display();
+    ball.display();
+    leftPaddle.display();
+    rightPaddle.display();
 
-  //Creates an animation for the point winner
-  //(Needs to be called on multiple frames)
-  if(leftPaddle.hasScored){
-    leftPaddle.winningAnimation();
-  }
+    //Creates an animation for the point winner
+    //(Needs to be called on multiple frames)
+    if(leftPaddle.hasScored){
+      leftPaddle.winningAnimation();
+    }
 
-  if(rightPaddle.hasScored){
-    rightPaddle.winningAnimation();
+    if(rightPaddle.hasScored){
+      rightPaddle.winningAnimation();
+    }
   }
 }
 
@@ -113,3 +132,54 @@ function checkForScores(){
     ball.reset();
   }
 }
+
+///////NEW////////
+function displayIntro(){
+  //setting some text
+  textFont(myFont);
+  textSize(50);
+  textAlign(CENTER,CENTER);
+
+  //detect is x key has been pressed
+  if(keyIsDown(88)){
+    xPressed = true;
+  }
+
+  //Play a 3 second countdown after key press
+  if(xPressed){
+    //the intro stops playing after the countdown is over
+    if(coundownTimer === 0){
+      introPlaying = false;
+    //During the countdown, paddle, ball and some text are displayed
+    }else{
+      ball.display();
+      leftPaddle.display();
+      rightPaddle.display();
+      fill(255);
+      textSize(50);
+
+      //The text changes dependant on the countdown timer
+      if(coundownTimer > 120){
+        text("3",width/2,height/2 - 100);
+      } else if(coundownTimer > 60) {
+        text("2",width/2,height/2 - 100);
+      } else {
+        text("1",width/2,height/2 - 100);
+      }
+
+      //decrease the countdown timer
+      coundownTimer--;
+    }
+
+  //if the x has not been pressed, display Title text
+  } else {
+    fill(random(200,255), constrain(textOpacity,0,255));
+    textSize(50);
+    text(titleText,width/2,height/2);
+    textSize(25);
+    text(startGameText, width/2, height/2 + 100);
+    textOpacity++; //text appears in a fade-in, thus with increasing opacity
+  }
+}
+
+//////END NEW///////
