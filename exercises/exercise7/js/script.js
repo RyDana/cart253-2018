c4faa8c9070b5efa5b765ed5e97255d9fc20b5c1
Dana Ryashy <danriachi@hotmail.com>
2018-11-27 14:03:08 -0500

    ex7: testing mobile fullscreen

--- exercises/exercise7/js/script.js
+++ exercises/exercise7/js/script.js
@@ -11,12 +11,21 @@ var onMobile = false;
 var rotationDetected = false;
 //detect if the canvas was created
 var canvasCreated = false;
+// Track whether we're in fullscreen
+var isFullScreen = false;
+// Track the ratio of width to height for the canvas
+var canvasRatio;
+// Track the canvas element
+var canvas;
 
 //Sounds
 var beepSound;
 
 //player
 var player;
+
+//text
+var putToLandscape = "Turn your screen to \n landscape mode";
 
 // preload()
 //
@@ -34,17 +43,20 @@ function preload() {
 function setup() {
   // Create canvas and set drawing modes
   if(onMobile){
-    createCanvas(window.innerWidth , window.innerHeight);
+    canvas = createCanvas(window.innerWidth , window.innerHeight);
     // Fill the background
     background(0, 216, 255);
     //create player using PlayerMobile constructor (for touch capabilities)
     player = new PlayerMobile(width/2, height/2);
 
   }else{
-    createCanvas(1280,480);
+    canvas = createCanvas(1280,480);
     //Create Player using Player constructor (for keyboard capabilities)
     player = new Player(width/2, height/2);
   }
+
+  // We calculate the display ratio of width to height for later
+  canvasRatio = width/height;
 
   rectMode(CENTER);
   noStroke();
@@ -58,7 +70,7 @@ function setup() {
 function draw() {
   //Asks user to put phone in ladscape mode if user on a phone
   if (onMobile && window.innerWidth < window.innerHeight && !rotationDetected){
-    text("turn your screen to landscape mode", width/2, height/2);
+    text(putToLandscape, width/2, height/2);
 
   } else{
     //recreate canvas to fit mobile device once rotated (called once)
@@ -136,4 +148,28 @@ function recreateCanvasOnMobile(){
     //do not ask to rotate the sceen again
     rotationDetected = true;
   }
+}
+
+function mousePressed() {
+  if(mouseX <width && mouseX > width*2/3 && mouseY > 0 && mouseY < height/4){
+    // When the mouse is pressed we toggle the variable tracking fullscreen
+    isFullScreen = !isFullScreen;
+    // And set fullscreen to the result
+    fullscreen(isFullScreen);
+  }
+
+  // Now we calculate the desired height of the canvas based on whether we're
+  // in fullscreen (and want displayHeight) or not (and want the regular height)
+  // var newHeight = 0;
+  // if (isFullScreen) {
+  //   newHeight = displayHeight;
+  // }
+  // else {
+  //   newHeight = height;
+  // }
+  // Finally, using p5.dom's style() method we set the height and width of the
+  // canvas element to the new height
+  // canvas.style("height:" + newHeight + "px");
+  // And we calculate and set the width based on the ratio
+  // canvas.style("width:" + newHeight * canvasRatio + "px");
 }
